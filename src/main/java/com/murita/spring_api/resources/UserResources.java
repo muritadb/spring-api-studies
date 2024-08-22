6b35e46298513a43456ec2a9e84fc321ea4f5427
Douglas Murita <douglasb008@gmail.com>
2024-08-21 22:48:00 -0300

    feat: service layer

--- src/main/java/com/murita/spring_api/resources/UserResources.java
+++ src/main/java/com/murita/spring_api/resources/UserResources.java
@@ -1,8 +1,14 @@
 package com.murita.spring_api.resources;
 
 import com.murita.spring_api.entities.User;
+import com.murita.spring_api.services.UserService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserResources {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L,"Douglas","any@mail.com", "9999999", "123");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
