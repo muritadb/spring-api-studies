c2def08165faed0c949d57faf3dbe696631ff444
Douglas Murita <douglasb008@gmail.com>
2024-08-24 17:52:07 -0300

    add enum order status

--- src/main/java/com/murita/spring_api/config/TestConfig.java
+++ src/main/java/com/murita/spring_api/config/TestConfig.java
@@ -3,7 +3,6 @@ package com.murita.spring_api.config;
 import java.time.Instant;
 import java.util.Arrays;
 
-import org.hibernate.mapping.Array;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.murita.spring_api.entities.Order;
 import com.murita.spring_api.entities.User;
+import com.murita.spring_api.entities.enums.OrderStatus;
 import com.murita.spring_api.repositories.OrderRepository;
 import com.murita.spring_api.repositories.UserRepository;
 
@@ -29,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
     User u1 = new User(null, "Douglas Barbosa", "d@mail.com", "99999", "123");
     User u2 = new User(null, "Jaque Santos", "jaque@mail.com", "888888", "123");
 
-    Order o1 = new Order(null, Instant.parse("2024-08-24T13:30:49Z"), u1);
-    Order o2 = new Order(null, Instant.parse("2024-07-29T20:10:20Z"), u2);
-    Order o3 = new Order(null, Instant.parse("2024-08-06T05:27:05Z"), u1);
+    Order o1 = new Order(null, Instant.parse("2024-08-24T13:30:49Z"), OrderStatus.PAID, u1);
+    Order o2 = new Order(null, Instant.parse("2024-07-29T20:10:20Z"), OrderStatus.WAITING_PAYMENT, u2);
+    Order o3 = new Order(null, Instant.parse("2024-08-06T05:27:05Z"), OrderStatus.WAITING_PAYMENT, u1);
 
     userRepository.saveAll(Arrays.asList(u1, u2));
     orderRepository.saveAll(Arrays.asList(o1, o2, o3));
