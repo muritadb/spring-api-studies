f08b46feb067bb9ce46fd273e790470f28f67695
Douglas Murita <douglasb008@gmail.com>
2024-08-26 21:46:01 -0300

    association many to many

--- src/main/java/com/murita/spring_api/entities/Category.java
+++ src/main/java/com/murita/spring_api/entities/Category.java
@@ -1,6 +1,9 @@
 package com.murita.spring_api.entities;
 
 import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.HashSet;
 
 import java.io.Serializable;
@@ -9,8 +12,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -22,7 +25,8 @@ public class Category implements Serializable {
   private Long id;
   private String name;
 
-  @Transient
+  @JsonIgnore
+  @ManyToMany(mappedBy = "categories")
   private Set<Product> products = new HashSet<>();
 
   public Category() {
