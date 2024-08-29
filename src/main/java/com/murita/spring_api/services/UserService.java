d91d7afd76fbab83af77c0dae65b7a35078abef0
Douglas Murita <douglasb008@gmail.com>
2024-08-28 23:41:15 -0300

    add resource not found exception

--- src/main/java/com/murita/spring_api/services/UserService.java
+++ src/main/java/com/murita/spring_api/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.murita.spring_api.entities.User;
 import com.murita.spring_api.repositories.UserRepository;
+import com.murita.spring_api.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
   public User findById(Long id) {
     Optional<User> obj = repository.findById(id);
-    return obj.get();
+    return obj.orElseThrow(() -> new ResourceNotFoundException(id));
   }
 
   public User insert(User obj) {
